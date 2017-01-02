27a924580421645b47a2d38400487fbd8adcb4a2
gjaegle <contact.gilles@me.com>
2017-01-02 13:50:38 +0100

    with build variables

--- Parity-Image-Builder/build_image.sh
+++ Parity-Image-Builder/build_image.sh
@@ -1,11 +1,14 @@
-host_chain_path=$HOME/chain
+host_chain_path=/home/gilles/chain
 container_chain_path=chain
 
+parity_source="http://d1h4xl4cr1h0mo.cloudfront.net/v1.4.7/aarch64-unknown-linux-gnu/parity_1.4.7_arm64.deb"
+parity_package=$(echo $parity_source | egrep -o "parity_[0-9]+.[0-9]+.[0-9]+_arm64.deb")
+
 parity_image_name="parity64"
-parity_version="1.4.7"
+parity_version=$(echo $parity_package | egrep -o "[0-9]+.[0-9]+.[0-9]+")
 docker_hub_orga="spdt"
 
-./build_image.sh $parity_image_name $parity_version
+docker build --no-cache --tag=$parity_image_name:$parity_version --build-arg PARITY_VERSION=$parity_version --build-arg PARITY_PKG_PATH=$parity_source --build-arg NODE_USER=$USER .
 
 docker tag $parity_image_name:$parity_version $docker_hub_orga/$parity_image_name:$parity_version
 docker push $docker_hub_orga/$parity_image_name:$parity_version
