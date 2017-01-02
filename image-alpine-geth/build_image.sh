a4c1be9a88e7a552a7f2d50acec690658ad2c028
gjaegle <contact.gilles@me.com>
2017-01-02 11:43:47 +0100

    Create build_image.sh

---
+++ image-alpine-geth/build_image.sh
@@ -0,0 +1,10 @@
+local_image_name="geth-alpine64"
+local_image_tag="1.5.5"
+remote_image_name="geth64"
+remote_image_tag=$local_image_tag-alpine
+docker_hub_orga="spdt"
+
+
+docker build --no-cache --tag=$local_image_name:$local_image_tag --build-arg GETH_VERSION=v$local_image_tag --build-arg NODE_USER=$USER .
+docker tag $local_image_name:$local_image_tag $docker_hub_orga/remote_image_name:$remote_image_tag
+docker push $docker_hub_orga/$remote_image_name:$remote_image_tag
