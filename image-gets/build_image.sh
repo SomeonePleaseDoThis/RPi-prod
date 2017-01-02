ef3d8e742d9dd98ddc2e38f064cc49a8993f13bf
gjaegle <contact.gilles@me.com>
2017-01-02 11:09:57 +0100

    update with Dockerfile arguments

--- image-gets/build_image.sh
+++ image-gets/build_image.sh
@@ -5,6 +5,6 @@ remote_image_tag=$local_image_tag-alpine
 docker_hub_orga="spdt"
 
 
-./build_image.sh $local_image_name $local_image_tag
+docker build --no-cache --tag=$local_image_name:$local_image_tag --build-arg GETH_VERSION=v$local_image_tag --build-arg NODE_USER=$USER .
 docker tag $local_image_name:$local_image_tag $docker_hub_orga/remote_image_name:$remote_image_tag
 docker push $docker_hub_orga/$remote_image_name:$remote_image_tag
