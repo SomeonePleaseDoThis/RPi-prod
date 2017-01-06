49cb4e8cb238b57569391cfded0c6e53146f4981
gjaegle <contact.gilles@me.com>
2017-01-06 17:49:54 +0100

    Update build_image.sh
    automatically push with tag "latest"

--- image-alpine-geth/build_image.sh
+++ image-alpine-geth/build_image.sh
@@ -7,4 +7,6 @@ docker_hub_orga="spdt"
 
 docker build --no-cache --tag=$local_image_name:$local_image_tag --build-arg GETH_VERSION=v$local_image_tag --build-arg NODE_USER=$USER .
 docker tag $local_image_name:$local_image_tag $docker_hub_orga/remote_image_name:$remote_image_tag
+docker tag $local_image_name:$local_image_tag $docker_hub_orga/remote_image_name:latest
 docker push $docker_hub_orga/$remote_image_name:$remote_image_tag
+docker push $docker_hub_orga/$remote_image_name:latest
