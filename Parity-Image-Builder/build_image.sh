45c199e25b62d921e15aed3564db2d44a80750e9
gjaegle <contact.gilles@me.com>
2017-01-06 23:15:42 +0100

    Update build_image.sh

--- Parity-Image-Builder/build_image.sh
+++ Parity-Image-Builder/build_image.sh
@@ -8,7 +8,12 @@ parity_image_name="parity64"
 parity_version=$(echo $parity_package | egrep -o "[0-9]+.[0-9]+.[0-9]+")
 docker_hub_orga="spdt"
 
-docker build --no-cache --tag=$parity_image_name:$parity_version --build-arg PARITY_VERSION=$parity_version --build-arg PARITY_PKG_PATH=$parity_source --build-arg NODE_USER=$USER .
+docker build --no-cache \
+  --tag=$parity_image_name:$parity_version \
+  --build-arg PARITY_VERSION=$parity_version \
+  --build-arg PARITY_PKG_PATH=$parity_source \
+  --build-arg NODE_USER=$USER \
+  https://github.com/SomeonePleaseDoThis/RPi-prod.git#master:Parity-Image-Builder
 
 docker tag $parity_image_name:$parity_version $docker_hub_orga/$parity_image_name:$parity_version
 docker push $docker_hub_orga/$parity_image_name:$parity_version
