c39200992513ecfef64707801ef2326145654fe6
gjaegle <contact.gilles@me.com>
2017-01-09 22:13:49 +0100

    Update setup_user.sh

--- setup_user.sh
+++ setup_user.sh
@@ -1,12 +1,12 @@
 #configure opensuse
 echo "set root password"
 passwd root
-echo $1 > /etc/hostname
-useradd -m -G wheel $0
-echo "set user pwd"
-passwd $0
+echo $2 > /etc/hostname
+useradd -m -G wheel $1
+echo "set $1 user pwd"
+passwd $1
 zypper in docker htop nano
 systemctl start docker
 docker run hello-world
 systemctl enable docker
-usermod -a -G docker $0
+usermod -a -G docker $1
