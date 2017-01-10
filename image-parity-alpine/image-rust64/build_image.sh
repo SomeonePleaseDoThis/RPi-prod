bbee69e6f088aa053fc5baa042819442a05b940f
gjaegle <contact.gilles@me.com>
2017-01-10 17:07:42 +0100

    Create build_image.sh

---
+++ image-parity-alpine/image-rust64/build_image.sh
@@ -0,0 +1,26 @@
+
+# TO BE MODIFIED
+#  AND
+# TO BE COMPLETED
+
+host_chain_path=/home/gilles/data/parity
+container_chain_path=chain
+
+parity_source="http://d1h4xl4cr1h0mo.cloudfront.net/v1.4.9/aarch64-unknown-linux-gnu/parity_1.4.9_arm64.deb"
+parity_package=$(echo $parity_source | egrep -o "parity_[0-9]+.[0-9]+.[0-9]+_arm64.deb")
+
+parity_image_name="parity64"
+parity_version=$(echo $parity_package | egrep -o "[0-9]+.[0-9]+.[0-9]+")
+docker_hub_orga="spdt"
+
+docker build --no-cache \
+  --tag=$parity_image_name:$parity_version \
+  --build-arg PARITY_VERSION=$parity_version \
+  --build-arg PARITY_PKG_PATH=$parity_source \
+  --build-arg NODE_USER=$USER \
+  https://github.com/SomeonePleaseDoThis/RPi-prod.git#master:Parity-Image-Builder
+
+docker tag $parity_image_name:$parity_version $docker_hub_orga/$parity_image_name:$parity_version
+docker tag $parity_image_name:$parity_version $docker_hub_orga/$parity_image_name:latest
+docker push $docker_hub_orga/$parity_image_name:$parity_version
+docker push $docker_hub_orga/$parity_image_name:latest
