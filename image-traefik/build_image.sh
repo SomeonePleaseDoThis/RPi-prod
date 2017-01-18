ce94f98569e88400b8cd8531a1dfe3abfaf817bd
gjaegle <contact.gilles@me.com>
2017-01-18 21:31:25 +0100

    Update build_image.sh

--- image-traefik/build_image.sh
+++ image-traefik/build_image.sh
@@ -5,12 +5,13 @@ docker_hub_orga="spdt"
 traefik_binary="https://github.com/containous/traefik/releases/download/v1.1.2/traefik_linux-arm64"
 traefik_version=$(echo $traefik_binary | egrep -o "[0-9]+.[0-9]+.[0-9]+")
 
+echo $USER builds traefik version $traefik_version
+
 local_image_tag=$traefik_version
 remote_image_tag=$local_image_tag
 
 docker build --no-cache --tag=$local_image_name:$local_image_tag \
-  --build-arg TRAEFIK_VERSION=v$local_image_tag \
-  --build-arg INST_PATH="" \
+  --build-arg INST_PATH=$traefik_binary \
   https://github.com/SomeonePleaseDoThis/RPi-prod.git#master:image-traefik
 docker tag $local_image_name:$local_image_tag $docker_hub_orga/$remote_image_name:$remote_image_tag
 docker tag $local_image_name:$local_image_tag $docker_hub_orga/$remote_image_name:latest
