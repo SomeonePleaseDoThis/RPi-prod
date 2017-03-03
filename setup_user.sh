73143307f66f7c721938c6f35b3dec9fe6d2abbb
gjaegle <contact.gilles@me.com>
2017-03-04 00:04:22 +0100

    corrected

--- setup_user.sh
+++ setup_user.sh
@@ -6,7 +6,7 @@ useradd -m -G wheel $1
 echo "set $1 user pwd"
 passwd $1
 zypper in docker htop nano
-systemctl start docker
-docker run hello-world
+service docker start
+docker run aarch64/hello-world
 systemctl enable docker
 usermod -a -G docker $1
