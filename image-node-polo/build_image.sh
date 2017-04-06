e57ee0ce75a0ba61401bb27aa51f16b6ced8b687
gjaegle <contact.gilles@me.com>
2017-04-06 09:27:54 +0200

    corrections

--- image-node-polo/build_image.sh
+++ image-node-polo/build_image.sh
@@ -1,13 +1,13 @@
 node_source="7.7.2-slim"
 node_version=$(echo ${node_source} | egrep -o "[0-9]+.[0-9]+.[0-9]+")
-echo $node_package
+echo ${node_package}
 
 node_image_name="polo64"
 echo ${node_version}
 docker_hub_orga="spdt"
 
 docker build --no-cache \
-  --tag=${node_image_name}:$p{node_version} \
+  --tag=${node_image_name}:${node_version} \
   --build-arg NODE_VERSION=${node_version} \
   --build-arg NODE_PKG_PATH=${node_source} \
   https://github.com/SomeonePleaseDoThis/RPi-prod.git#master:image-node-polo
